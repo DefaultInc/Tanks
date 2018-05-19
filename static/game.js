1c6f3192cf082a226e64d5022808e427f5dfbb8d
Ильяс Сабешулы <emailforanypurposes@gmail.com>
2018-05-19 15:32:19 +0600

    added assets

--- static/game.js
+++ static/game.js
@@ -1,21 +1,25 @@
 GAME_INTERVAL = 500
 FIELD = "FIELD"
-var c=new Image(100,100)
-c.src = "/static/tankBlack_outline.png?x76964"
+var c = new Image(100, 100)
+c.src = "/static/tank.png?x76964"
 document.body.appendChild(c);
 c.style.position = "absolute"
-players = [{e: c, x: 100, y: 10}]
+players = [{
+  e: c,
+  x: 100,
+  y: 10
+}]
 
 var socket = io();
-socket.on('game', function(data) {
+socket.on('game', function (data) {
 
 });
 
 function DRAW() {
-	players.forEach(player => {
-		player.e.style.left = player.x+'px';
-		player.e.style.top = player.y+'px';
-	})
+  players.forEach(player => {
+    player.e.style.left = player.x + 'px';
+    player.e.style.top = player.y + 'px';
+  })
 }
 
 function sendState() {
@@ -23,16 +27,16 @@ function sendState() {
 }
 
 function update() {
-	players[0].x += 10
-	DRAW()
+  players[0].x += 10
+  DRAW()
 }
 
-setInterval(function() {
-	update();
-	sendState();
+setInterval(function () {
+  update();
+  sendState();
 }, GAME_INTERVAL);
 
-document.addEventListener('keydown', function(event) {
+document.addEventListener('keydown', function (event) {
   switch (event.keyCode) {
     case 65: // A
       movement.left = true;
