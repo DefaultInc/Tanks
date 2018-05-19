9605db6610b4bb5c470e2479a8f9b87c100b05c9
amzedauren <dauren.amze1213@gmail.com>
2018-05-19 23:20:41 +0600

    bot

--- static/game.js
+++ static/game.js
@@ -1,17 +1,12 @@
-GAME_INTERVAL = 50
-FIELD = "FIELD"
-PLAYER_ID = "1"
-DIR = ""
-STEP = 10
-movement = {}
-var c = new Image(100, 100)
-c.src = "/static/tank.png?x76964"
-document.body.appendChild(c);
-c.style.position = "absolute"
+GAME_INTERVAL = 50;
+tankStep = 10;
+
+var playerIds = 0
+bullets = []
+players = []
 
 
-
-function newBullet(direct, x, y) {
+function newBullet(player, direct, x, y) {
   let b = new Image(25, 30);
   b.src = "/static/bullet.png?x76964"
   b.style.position = "absolute"
@@ -20,6 +15,7 @@ function newBullet(direct, x, y) {
 
   return {
     dir: direct,
+    own: player,
     b: b,
     x: x,
     y: y
@@ -27,55 +23,38 @@ function newBullet(direct, x, y) {
 
 }
 
-players = [{
-  id: PLAYER_ID,
-  dir: "",
-  e: c,
-  x: 100,
-  y: 10
-}]
 
-var socket = io.connect()
-socket.on('game', function (tanks) {
-  players.forEach(function (player, index, players) {
-    let tank = tanks[player.id]
-    if (tank) {
-      let id = player.id
-      if (tank === "_left")
-        players[index].x += STEP
-      if (tank === "_right")
-        players[index].x -= STEP
-      if (tank === "_top")
-        players[index].y += STEP
-      if (tank === "_down")
-        players[index].y -= STEP
-    }
-  })
-  update()
-});
+function getNewPlayer(x, y, name) {
+  let c = new Image(100, 100)
+  c.src = "/static/tank.png?x76964"
+  document.body.appendChild(c);
+  c.style.position = "absolute"
+
+  return {
+    id: playerIds++,
+    name: name,
+    dir: "",
+    e: c,
+    x: x,
+    y: y
+  }
+}
 
 function DRAW() {
   players.forEach(player => {
     player.e.style.left = player.x + 'px';
     player.e.style.top = player.y + 'px';
-    if (movement.dir != undefined) {
-      player.e.src = 'static/tank' + movement.dir + '.png';
+    if (player.dir != undefined) {
+      player.e.src = 'static/tank' + player.dir + '.png';
     }
   })
 }
 
-function sendState() {
-  socket.emit("game", {
-    id: PLAYER_ID,
-    dir: DIR
-  });
-}
+function canMove(element) {
+  var x = element.x;
+  var y = element.y;
 
-function canMove() {
-  var x = players[0].x;
-  var y = players[0].y;
-
-  switch (movement.dir) {
+  switch (element.dir) {
     case "":
       if (y + 100 + 10 > 1024 || map[x][y + 100 + 10] == 1 || map[x + 100][y + 100 + 10] == 1 || y + 10 >= 2024)
         return false;
@@ -98,48 +77,38 @@ function canMove() {
 }
 
 function update() {
-
-  if (canMove()) {
-    switch (movement.dir) {
-      case "_left": // A
-        players[0].x -= 10
-        break;
-      case "_up": // W
-        players[0].y -= 10
-        break;
-      case "_right": // D
-        players[0].x += 10
-        break;
-      case "": // S
-        players[0].y += 10
-        break;
+  
+  players.forEach(element => {
+    if (canMove(element)) {
+      switch (element.dir) {
+        case "_left": // A
+          element.x -= 10
+          break;
+        case "_up": // W
+          element.y -= 10
+          break;
+        case "_right": // D
+          element.x += 10
+          break;
+        case "": // S
+          element.y += 10
+          break;
+      }
     }
-  }
-
+  });
   DRAW()
 }
 
-
-function shoot(x, y) {
-  let newb = newBullet(movement.dir, x, y);
+function shoot(player) {
+  let newb = newBullet(player.id, player.dir, player.x + 50 - 12, player.y + 50 - 15);
   bullets.push(newb)
   document.body.appendChild(newb.b);
 
-  // var x, y;
-  // document.body.appendChild(b);
-  // players.forEach(player => {
-  //   console.log(player);
-  //   x = player.x;
-  //   y = player.y;
-  // });
-  // bullets.forEach(bullet => {
-  //   bullet.s.style.left = x + 15 + 'px';
-  //   bullet.s.style.top = y + 'px';
-  // });
 }
 
 function updateBullets() {
   to_remove = []
+
   bullets.forEach(bullet => {
 
     switch (bullet.dir) {
@@ -159,11 +128,36 @@ function updateBullets() {
     bullet.b.style.left = bullet.x + 'px'
     bullet.b.style.top = bullet.y + 'px';
 
-    if (bullet.x < 0 || bullet.y < 0 || bullet.x > 1024 || bullet.y > 1024) {
+    if (bullet.x < 0 || bullet.y < 0 || bullet.x > 1024 || bullet.y > 1024 || map[bullet.x][bullet.y] == 1) {
       document.body.removeChild(bullet.b);
       to_remove.push(bullet)
+    } else {
+      players.forEach(element => {
+        if (bullet.own != element.id && intersect(element, bullet.x, bullet.y)) {
+          killPlayer(element);
+        }
+      });
     }
+
   });
+
+  function killPlayer(element){
+    
+    alert("WINNER: " + element.name)
+    location.reload();
+    var index = players.indexOf(element);
+    if (index > -1) {
+      players.splice(index, 1);
+    }
+    document.body.removeChild(element.e);
+  }
+
+
+  function intersect(player, x, y) {
+    if (player.x <= x && x <= player.x + 100 &&
+      player.y <= y && y <= player.y + 100)
+      return true;
+  }
 
   to_remove.forEach(ele => {
     var index = bullets.indexOf(ele);
@@ -174,36 +168,85 @@ function updateBullets() {
 
 }
 
+setInterval(function () {
+  move_bot();
+}, 1000);
+  
+
 
 setInterval(function () {
-  sendState();
+  update();
 }, GAME_INTERVAL);
 
 // update bullets
 setInterval(function () {
   updateBullets()
-}, GAME_INTERVAL / 2);
-
-
+}, GAME_INTERVAL / 4);
 
 document.addEventListener('keydown', function (event) {
   switch (event.keyCode) {
     case 65: // A
-      movement.dir = "_left";
+      players[0].dir = "_left";
       break;
     case 87: // W
-      movement.dir = "_up";
+      players[0].dir = "_up";
       break;
     case 68: // D
-      movement.dir = "_right";
+      players[0].dir = "_right";
       break;
     case 83: // S
-      movement.dir = "";
+      players[0].dir = "";
+      break;
+    case 37: // A
+      players[1].dir = "_left";
+      break;
+    case 38: // W
+      players[1].dir = "_up";
+      break;
+    case 39: // D
+      players[1].dir = "_right";
+      break;
+    case 40: // S
+      players[1].dir = "";
       break;
     case 32: // Spacebar
-      shoot(players[0].x, players[0].y);
+      shoot(players[0]);
       break;
+    case 13:
+      shoot(players[1]);
+      break
   }
 });
 
-movement.dir = ""
+
+
+players.push(getNewPlayer(10,10, 'BOT'))
+
+players.push(getNewPlayer(500,10, 'USER'))
+
+var bot_dirs = ["_left", "_right", "_up", ""]
+var steps = 0;
+
+function can_see() {
+  if (Math.abs(players[0].x - players[1].x) < 200 || Math.abs(players[0].y - players[1].y) < 200) return true;
+  return false;
+}
+
+function move_bot() {
+  if (can_see()) {
+    if (players[0].dir == '_left')
+      players[1].dir = '_right';
+    if (players[0].dir == '_right')
+      players[1].dir = '_left';
+    if (players[0].dir == '_top')
+      players[1].dir = '_bot';
+    if (players[0].dir == '_bot')
+      players[1].dir = '_left';
+  }
+  if (!canMove(players[1])) {
+    steps++;
+    players[1].dir = bot_dirs[Math.round(steps) % 4];
+
+  }
+  shoot(players[1]);  
+}
