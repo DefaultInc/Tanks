6f66018477569e2072903d34154e225d5b7de828
NurlashKO <zh.nurlan96@gmail.com>
2018-05-19 16:02:08 +0600

    assets fix

--- static/game.js
+++ static/game.js
@@ -1,10 +1,16 @@
 GAME_INTERVAL = 500
 FIELD = "FIELD"
+PLAYER_ID = "1"
+STEP = 10
+movement = {}
 var c = new Image(100, 100)
 c.src = "/static/tank.png?x76964"
 document.body.appendChild(c);
 c.style.position = "absolute"
+
 players = [{
+  id: PLAYER_ID,
+  dir: "down",
   e: c,
   x: 100,
   y: 10
@@ -12,7 +18,26 @@ players = [{
 
 var socket = io();
 socket.on('game', function (data) {
+	console.log("asd")
+	players.forEach(function(player, index, players) {
+		if (player.id in data) {
+			let id = player.id
+			console.log(data, player)
+			if (data[id] != player.dir) {
+				player.dir = data[id]
+			} else {
+				if (data[id] == "left")
+					player.x += STEP
+				if (data[id] == "right")
+					player.x -= STEP
+				if (data[id] == "top")
+					player.y += STEP
+				if (data[id] == "down")
+					player.y -= STEP
 
+			}
+		}
+	})
 });
 
 function DRAW() {
@@ -23,7 +48,7 @@ function DRAW() {
 }
 
 function sendState() {
-
+	socket.emit('game', {PLAYER_ID: movement})
 }
 
 function update() {
